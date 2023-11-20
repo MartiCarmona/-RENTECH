1bc07758f1847dd246e989080f363b7fdbfa1e0e
Marti Carmona <mtkindus@gmail.com>
2023-11-20 16:44:25 +0100

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,3 +10,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+

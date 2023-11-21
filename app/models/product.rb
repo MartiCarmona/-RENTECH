0654b8d0adbe4cedce7b5dde70477b9dba5e07d8
Nahuel Ribera <nriberapingitore@gmail.com>
2023-11-21 13:29:16 +0100

    form-n

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,16 @@ class Product < ApplicationRecord
   has_many :bookings
   has_many :chats
   has_one_attached :image
+
+  validate :image_format
+
+  private
+
+  def image_format
+    return unless image.attached?
+
+    unless image.content_type.in?(%w[image/jpeg image/jpg image/png])
+      errors.add(:image, 'must be a JPEG, JPG, or PNG')
+    end
+  end
 end

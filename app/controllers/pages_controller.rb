491913fde105395c5942b354e7e1000bc7a2cfd7
Nahuel Ribera <nriberapingitore@gmail.com>
2023-11-21 15:55:50 +0100

    test-n

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @products = Product.all.order(created_at: :desc).limit(5) # Adjust the limit as needed
+    @products = Product.all # Adjust the limit as needed
   end
 end

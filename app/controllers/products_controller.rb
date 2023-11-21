2bb0113e94283a00f631841b3f28e4a216577fe1
Nahuel Ribera <nriberapingitore@gmail.com>
2023-11-21 13:17:35 +0100

    form-n

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,34 @@
+# app/controllers/products_controller.rb
+
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
+  end
+
+  def create
+    @product = current_user.products.new(product_params)
+
+    if @product.save
+      flash[:success] = "Product successfully created!"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :condition, :image)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   has_many :rentals
   has_many :bookings
   has_many :chats
+  has_one_attached :image
 end

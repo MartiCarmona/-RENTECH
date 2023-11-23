7831b00bf20432bbfcd255100a08796060a62a9c
Ignacio Borrell <ignacioborrellr@gmail.com>
2023-11-23 15:10:15 +0100

    luciatusia

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,15 +2,15 @@ class RentalsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @product = Product.find(params[:id])
+    @product = Product.find(params[:product_id])
 
     if current_user.rented_products.exclude?(@product)
       Rental.create(product: @product, user: current_user)
       flash[:notice] = "Product rented successfully!"
+      redirect_to user_path(current_user)
     else
       flash[:alert] = "You already rented this product."
+      redirect_to product_path(@product)
     end
-
-    redirect_to @product
   end
 end

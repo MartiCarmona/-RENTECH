afac35e000c66b21c31def190a1d666a10c9da99
Marti Carmona <mtkindus@gmail.com>
2023-11-24 15:22:31 +0100

    Navbar profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_195249) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_24_134059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,12 +54,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_195249) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -73,8 +67,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_195249) do
   end
 
   create_table "rentals", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "product_id", null: false
+    t.bigint "user_id"
+    t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_rentals_on_product_id"

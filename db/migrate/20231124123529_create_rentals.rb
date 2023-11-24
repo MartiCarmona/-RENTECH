8e28ef41bce6b9c4073bce73ef7dc301dedb9e26
Ignacio Borrell <ignacioborrellr@gmail.com>
2023-11-24 15:18:48 +0100

    favourites

--- db/migrate/20231124123529_create_rentals.rb
+++ db/migrate/20231124123529_create_rentals.rb
@@ -1,9 +1,6 @@
-# db/migrate/[timestamp]_create_rentals.rb
-
-class CreateRentals < ActiveRecord::Migration[6.0] # or your Rails version
+class CreateRentals < ActiveRecord::Migration[7.1]
   def change
     create_table :rentals do |t|
-      # Define your columns here, for example:
       t.references :user, foreign_key: true
       t.references :product, foreign_key: true
       t.timestamps
